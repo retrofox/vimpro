e77cc5dce5e25fab7eebe436ccee5f69eb7d6302
Damian Suarez <rdsuarez@gmail.com>
2014-01-20 18:14:54 -0300

    rename sexvim by vimpro

--- install.sh
+++ install.sh
@@ -10,20 +10,20 @@ vim_dir="$dir/.vim"
 vim_bck_dir="$dir/._vim_bck"
 
 current=${PWD}
-sexvim_dir="$current/.vim/"
-sexvim_rc="$current/.vimrc"
-
-mkdir "$vim_dir/undo"
-chmod 777 "$vim_dir/undo"
+vimpro_dir="$current/.vim/"
+vimpro_rc="$current/.vimrc"
 
 # vim dir
 if [ -d "$vim_dir" ]; then 
   mv "$vim_dir" "$vim_bck_dir"
 fi
-ln -s $sexvim_dir $vim_dir
+ln -s $vimpro_dir $vim_dir
 
 # vim_rc
 if [ -f "$vim_rc" ]; then
   mv "$vim_rc" "$vim_bck_rc"
 fi
-ln -s $sexvim_rc $vim_rc
+ln -s $vimpro_rc $vim_rc
+
+mkdir "$vim_dir/undo"
+chmod 777 "$vim_dir/undo"
